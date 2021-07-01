cacb18bb7086e2930b34e178a8a795d9638d47c6
pangestuf <farrasp17@gmail.com>
2021-07-01 09:44:02 +0700

    multi client

---
+++ chat/client1.py
@@ -0,0 +1,37 @@
+import socket
+import select
+import sys
+from threading import Thread
+from ftplib import FTP
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.connect(('127.0.0.1', 8081))
+
+name = "wong"
+
+def send_msg(sock):
+	while True:
+		data = input()
+		
+		sock.send((name + ' ' + data).encode())
+		sys.stdout.flush()
+
+def recv_msg(sock):
+	while True:
+		data = sock.recv(2048)
+		sys.stdout.write(data.decode() + '\n')
+
+Thread(target=send_msg, args=(server,)).start()
+# Thread(target=recv_msg, args=(server,)).start()
+
+while True:
+	sockets_list = [server]
+	read_socket, write_socket, error_socket = select.select(sockets_list, [], [])
+	for socks in read_socket:
+		send_msg(socks)
+		# if socks == server:
+		# 	recv_msg(socks)
+		# # else:
+		# # 	send_msg(socks)
+
+server.close()
--- chat/server.py
+++ chat/server.py
@@ -1,29 +1,50 @@
-import socket 
+import socket
+import select
+import sys
 import threading
 
-class ServerNode:
-    def __init__(self):
-        self.node = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        port_and_ip = ('127.0.0.1', 12345)
-        self.node.bind(port_and_ip)
-        self.node.listen(5)
-        self.connection, addr = self.node.accept()
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+ip_address = '127.0.0.1'
+port = 8081
+server.bind((ip_address, port))
+server.listen(100)
+list_of_clients = []
 
-    def send_sms(self, SMS):
-        self.connection.send(SMS.encode())
+BUFFER_SIZE = 2048
+sep = "|"
 
-    def receive_sms(self):
-        while True:
-            data = self.connection.recv(1024).decode()
-            print(data)
+def clientthread(conn, addr):
+	while True:
+		try:
+			data = conn.recv(2048).decode()
+			if data:
+				print('<' + data.split(" ")[0] + '>' + data.partition(' ')[2])
+				message_to_send = '<' + addr[0] + '>' + data
+				broadcast(message_to_send, conn)
+			else:
+				remove(conn)
+		except:
+			continue
 
-    def main(self):
-        while True:
-            message = input()
-            self.send_sms(message)
+def broadcast(message, connection):
+	for clients in list_of_clients:
+		if clients != connection:
+			try:
+				clients.send(message.encode())
+			except:
+				clients.close()
+				remove(clients)
 
-server = ServerNode()
-always_receive = threading.Thread(target=server.receive_sms)
-always_receive.daemon = True
-always_receive.start()
-server.main()
+def remove(connection):
+	if connection in list_of_clients:
+		list_of_clients.remove(connection)
+
+while True:
+	conn, addr = server.accept()
+	list_of_clients.append(conn)
+	data = conn.recv(2048).decode()
+	print(data.split(" ")[0] + ' connected')
+	threading.Thread(target=clientthread, args=(conn, addr)).start()
+
+conn.close()
