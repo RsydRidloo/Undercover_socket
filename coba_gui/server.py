0b4ec8679757084afee2fde1d310b4a78d5e4d6f
hrswcksono <hariswicaksono250999@gmail.com>
2021-07-16 10:22:50 +0700

    fix

--- coba_gui/server.py
+++ coba_gui/server.py
@@ -20,7 +20,7 @@ civillian_words = ['sprite', 'macan', 'gado-gado', 'indomaret', 'kerbau']
 turn = 0
 i = 0
 x = 0
-y = 1
+y = 0
 idx = 0
 old_remaining_client = 0
 
@@ -36,10 +36,10 @@ def clientthread(conn, addr):
     while True:
         try:
             data = conn.recv(2048).decode()
-            # print(data)
+            print(data)
             if data:
                 if data.split("/")[0] == "joined":
-                    # print(data)
+                    print(data)
                     name_of_clients.append(data.split("/")[1])
                     # data = data.split("/")[1] + " joined"
                     broadcast(data, conn)
@@ -53,36 +53,35 @@ def clientthread(conn, addr):
                         role_of_clients = numpy.ones(number_of_clients)
                         role_of_clients[(number_of_clients-2):] = 2
                         random.shuffle(role_of_clients)
-                        # print(role_of_clients)
+                        print(role_of_clients)
                     else:
                         role_of_clients = numpy.ones(number_of_clients)
                         role_of_clients[(number_of_clients-1):] = 2
                         random.shuffle(role_of_clients)
-                        # print(role_of_clients)
+                        print(role_of_clients)
                     role_of_clients = role_of_clients.tolist()
                     for clients in list_of_clients:
-                        # print(clients)
+                        print(clients)
                         if role_of_clients[i] == 2:
                             word_to_send = "word/" + undercover_words[index]
                         else:
                             word_to_send = "word/" + civillian_words[index]
-                        # print(word_to_send)
+                        print(word_to_send)
                         clients.send(word_to_send.encode())
                         i = i + 1
                     sendToAll("clue_turn/" + remaining_clients[0], conn)
                     # print(name_of_clients[0])
                 elif data.split("/")[0] == "clue":
-                    # if y == 0:                        
-                    #     amount_of_client = len(remaining_clients)
-                    # else:
-                    #     amount_of_client = old_remaining_client
-                    amount_of_client = clientCounter(len(name_of_clients), y)
+                    if y == 0:                        
+                        amount_of_client = len(remaining_clients)
+                    else:
+                        amount_of_client = old_remaining_client
                     turn += 1
                     idx += 1
-                    # print(turn)
+                    print(turn)
                     broadcast("clue_send/" + data.split("/")[1], conn)
                     if turn - amount_of_client == 0:
-                        y += 1
+                        y = 1
                         idx = 0
                         sendToAll("discussion/", conn)
                         time.sleep(10)
@@ -91,41 +90,41 @@ def clientthread(conn, addr):
                         sendToAll("clue_turn/" + remaining_clients[idx], conn)
                 elif data.split("/")[0] == "vote":
                     voted_client.append(data.split("/")[1])
-                    # print(voted_client)
-                    # print(len(remaining_clients))
-                    # print(len(voted_client))
+                    print(voted_client)
+                    print(len(remaining_clients))
+                    print(len(voted_client))
                     if len(remaining_clients) == len(voted_client):
                         name = most_frequent(voted_client)
-                        # old_remaining_client = len(remaining_clients) + len(remaining_clients) - 1
+                        old_remaining_client = len(remaining_clients) + len(remaining_clients) - 1
                         index = name_of_clients.index(name)
                         remaining_clients.remove(name)
-                        # print(name)
-                        # print(index)
-                        # print(role_of_clients)
-                        # print(type(remaining_clients))
-                        # print(type(role_of_clients))
+                        print(name)
+                        print(index)
+                        print(role_of_clients)
+                        print(type(remaining_clients))
+                        print(type(role_of_clients))
                         if(role_of_clients[index] == 2):
                             role = "undercover"
                         else:
                             role = "civillian"
-                        # print(role)
-                        # print(type(role_of_clients))
+                        print(role)
+                        print(type(role_of_clients))
                         role_of_clients.pop(index)
                         most_voted_client = "mostVoted/" + role + "/" + name
-                        # print(most_voted_client)
+                        print(most_voted_client)
                         sendToAll(most_voted_client, conn)
                         if len(remaining_clients)<3:
-                            # print("test1")
+                            print("test1")
                             if 2 not in role_of_clients:
                                 sendToAll("civillian_win/", conn)
-                                # print("civillian_win")
+                                print("civillian_win")
                             else:
                                 sendToAll("undercover_win/", conn)
-                                # print("undercover_win")
+                                print("undercover_win")
                             break
                         if 2 not in role_of_clients:
                             sendToAll("civillian_win/", conn)
-                            # print("civillian_win")
+                            print("civillian_win")
                             break
                         # del remaining_roles[index]
                         voted_client.clear()
@@ -140,14 +139,10 @@ def clientthread(conn, addr):
         except:
             continue
 
-def clientCounter(client, index) :
-    result = client + (index-1)*(client-1) + ((index-1)*(index-2)*(-1))/2
-    return result
-
 def waitUntil(condition, output): #defines function
     wU = True
     while wU == True:
-        # print(condition)
+        print(condition)
         if condition: #checks the condition
             output
             wU = False
@@ -180,7 +175,7 @@ def remove(connection):
 while True:
     conn, addr = server.accept()
     list_of_clients.append(conn)
-    # print(list_of_clients)
+    print(list_of_clients)
     # print(addr[0] + ' connected')
     threading.Thread(target=clientthread, args=(conn, addr)).start()
 
